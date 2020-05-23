960064b6dadb73f8752705a19316e06d7bb34ced
lekhi <danek@poczta.onet.eu>
2020-05-23 17:53:58 +0200

    utworzenie modeli i pierwsza migracja

---
+++ cw11/cw11/Controllers/ClinicController.cs
@@ -0,0 +1,28 @@
+﻿using cw11.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw11.Controllers
+{
+    [Route("api/clinic")]
+    [ApiController]
+    public class ClinicController : ControllerBase
+    {
+        private readonly ClinicDbContext _context;
+
+        public ClinicController(ClinicDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetMedicament()
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ cw11/cw11/Models/ClinicDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw11.Models
+{
+    public class ClinicDbContext : DbContext
+    {
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Medicament> Medicaments { get; set; }
+        public DbSet<Patient> Patiens { get; set; }
+        public DbSet<Prescription> Prescriptions { get; set; }
+        public DbSet<Prescription_Medicament> Prescription_Medicaments { get; set; }
+
+
+        public ClinicDbContext() { }
+
+        public ClinicDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+    }
+}
